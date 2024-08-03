08d16fe98200ab0ec61869464c8532f505f1be3e
Utsav Gohel <74051841+UtsavGohel@users.noreply.github.com>
2024-08-03 19:49:53 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 let todos = [
   { id: 1, task: "Learn Node.js", completed: false },
   { id: 2, task: "Build a to-do app", completed: false },
-  { id: 3, task: "Build a to-do app with node.js", completed: false },
+  { id: 3, task: "Build a to-do app with node.js", completed: true },
 ];
 
 module.exports = todos;
